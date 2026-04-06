ecc4aab4c9b9a4c055f579c95fe7df68273394e2
Julie Rodriguez <julie@Julies-MacBook-Air.local>
2026-04-06 14:49:37 -0400

    Downgrade to Prisma v5 for Vercel compatibility

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL!
-  const adapter = new PrismaPg(connectionString)
-  return new PrismaClient({ adapter })
-}
-
-export const prisma = global.prisma ?? createPrismaClient()
+export const prisma = global.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma
